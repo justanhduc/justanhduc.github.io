fdde5bc0196561785bccfaf4651cb098049278e3
Duc <adnguyen@yonsei.ac.kr>
2020-12-31 18:35:48 +0900

    added js

---
+++ assets/js/page.js
@@ -0,0 +1,33 @@
+// highlight current menu item based on url
+// adapted from https://www.infoworld.com/article/3304440/setting-an-active-menu-item-based-on-the-current-url-with-jquery.html
+$(function () {
+    var path = window.location.pathname;
+    path = path.replace(/\/$/, "");
+    path = decodeURIComponent(path);
+    $(".heading .container .main-nav a").each(function () {
+        var href = $(this).attr('href');
+        if (href.length > 1) {
+            if (path.substring(0, href.length) === href) {
+                $(this).addClass('active');
+            }
+        } else if (path.length == 0) {
+            $(this).addClass('active');
+        }
+    });
+});
+
+// responsive menu bar
+$(function () {
+    $(".heading .container .icon").click(function () {
+        $(".heading .container nav").toggle()
+    })
+
+    $( window ).resize(function() {
+        if ($(".heading .container .icon").is(":visible")) {
+            $(".heading .container nav").hide()
+        } else {
+            $(".heading .container nav").show()
+        }
+    })
+
+})
